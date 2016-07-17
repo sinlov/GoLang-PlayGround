d3a3d0f2b9c1a2a3bb39fa94666e1a6cc66b8c2e
sinlov <sinlovgmppt@gmail.com>
2016-07-17 11:26:09 +0800

    add number format at golang play ground

--- Go2Me/go_base/go_var.go
+++ Go2Me/go_base/go_var.go
@@ -5,8 +5,6 @@ const v1 = 1
 const v2 = 2
 const v3 = 3
 
-var varibaleName Type = "value"
-
 var vname1, vname2, vname3 = v1, v2, v3
 
 // only use in function
@@ -17,8 +15,18 @@ var vname1, vname2, vname3 = v1, v2, v3
 // _（下划线）是个特殊的变量名，任何赋予它的值都会被丢弃
 var _, b = 34, 35
 
-// Go对于已声明但未使用的变量会在编译阶段报错，比如下面的代码就会产生一个错误
 
+// Go对于已声明但未使用的变量会在编译阶段报错，比如下面的代码就会产生一个错误
 func main() {
 	var i int
+}
+
+var isActivity bool
+var enable, disable = true, false
+
+func go_var_test_bool()  {
+	var available bool
+	valid := false
+	available = false
+	print(available, valid)
 }
