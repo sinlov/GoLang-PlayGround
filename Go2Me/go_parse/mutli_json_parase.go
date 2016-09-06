432c4e5e968cfafd3be78c8c6ae8e7a23e213c7f
sinlov <sinlovgmppt@gmail.com>
2016-09-06 20:14:59 +0800

    add mutil json test for simple-json and json4g

--- Go2Me/go_parse/mutli_json_parase.go
+++ Go2Me/go_parse/mutli_json_parase.go
@@ -4,34 +4,62 @@ import (
 	"encoding/json"
 	"github.com/bitly/go-simplejson"
 	"fmt"
+	"github.com/donnie4w/json4g"
 )
 
 var json_data = `{"status":200,"msg":"\u6210\u529f","data":[{"RemotePort":"","Subdomain":"sunny","Hostname":"","HttpAuth":"","Proto":{"http":"192.168.2.192:80","tcp":"192.168.2.192:80"}}]}`
 
 type Proto struct {
 	Http string `json:"http"`
-	Tcp string `json:"tcp"`
+	Tcp  string `json:"tcp"`
 }
 
 type Info struct {
 	RemotePort string `json:"RemotePort"`
-	Subdomain string `json:"Subdomain"`
-	Hostname string `json:"Hostname"`
-	HttpAuth string `json:"HttpAuth"`
-	Proto map[string]Proto `json:"Proto"`
+	Subdomain  string `json:"Subdomain"`
+	Hostname   string `json:"Hostname"`
+	HttpAuth   string `json:"HttpAuth"`
+	Proto      map[string]Proto `json:"Proto"`
 }
 
-type Body struct{
+type Body struct {
 	Status int `json:"status"`
-	Msg string `json:"msg"`
-	Data []Info `json:"data"`
+	Msg    string `json:"msg"`
+	Data   []Info `json:"data"`
 }
 
-func Test_mutli_json_double()  {
-	body, err := json.Marshal(json_data)
+func Test_mutli_json_double() {
+	js, err := simplejson.NewJson([]byte (json_data))
+	first, err := json.Marshal(js)
 	if err != nil {
-		panic(err.Error())
+		fmt.Println("json err:", err)
 	}
-	jsObj, err := simplejson.NewJson(body)
-	fmt.Println(jsObj)
+	fmt.Println("json.Marshal", string(first))
+	js.Set("status", nil)
+	fmt.Println(js.Get("status"))
+	fmt.Println(js)
+	sec, err := json.Marshal(js)
+	if err != nil {
+		fmt.Println("json err:", err)
+	}
+	fmt.Println("Set and Get", string(sec))
+	js.Del("status")
+	thr, err := json.Marshal(js)
+	if err != nil {
+		fmt.Println("json err:", err)
+	}
+	fmt.Println("Del", string(thr))
+	res, err :=js.MarshalJSON()
+	fmt.Println("MarshalJSON", string(res))
+
+
+	js4g,err := json4g.LoadByString(json_data)
+	fmt.Println("LoadByString", js4g.ToString())
+	js4g.GetNodeByPath("status").SetValue(403)
+	fmt.Println(js4g.ToString())
+	err = js4g.DelNode("status")
+	if  err != nil{
+		fmt.Println(err.Error())
+	}
+	fmt.Println("DelNode", js4g.ToString())
 }
