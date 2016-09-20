4f1494f5f14e4e8797ebfb84e00cc311e0f3965e
sinlov <sinlovgmppt@gmail.com>
2016-09-20 23:15:56 +0800

    add run_count for test string join

---
+++ Go2Me/go_time/run_count.go
@@ -0,0 +1,15 @@
+package go_time
+
+import (
+	"time"
+	"strings"
+	"fmt"
+)
+
+func FastTimeTest(slice []string) interface{} {
+	start_time := time.Now()
+	str := strings.Join(slice, " ")
+	end_time := time.Since(start_time).Nanoseconds()
+	fmt.Println("FastTimeTest res: ", str)
+	return end_time
+}
