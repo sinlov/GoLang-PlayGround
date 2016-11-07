2bdb19835dc95538384aa3c3d18636183f6b78b2
sinlov <sinlovgmppt@gmail.com>
2016-11-07 10:53:36 +0800

    play go routine

---
+++ Go2Me/go_routine/go_routine_out_time.go
@@ -0,0 +1,41 @@
+package go_routine
+
+import (
+	"time"
+	"fmt"
+)
+
+// 默认 5 秒超时
+const DEFAULT_TIME_OUT time.Duration = time.Second * 5
+
+func count_sum_out(start, end int, ch chan string, is_time_out bool) int {
+	var sum int = 0
+	for i := start; i < end; i++ {
+		sum += i
+	}
+	if is_time_out {
+		time.Sleep(time.Second * 8)
+	} else {
+		time.Sleep(time.Second * 3)
+	}
+	ch <- fmt.Sprintf("Sum from %d to %d is %d\n", start, end, sum)
+	return sum
+}
+
+func try_count_sum_out(is_time_out bool) {
+	var ch = make(chan string, 1)
+	var timeout = make(chan bool, 1)
+
+	go count_sum_out(1, 10, ch, is_time_out)
+	go func() {
+		time.Sleep(DEFAULT_TIME_OUT)
+		timeout <- true
+	}()
+
+	select {
+	case sum := <-ch:
+		fmt.Print(sum)
+	case <-timeout:
+		fmt.Printf("Sorry, TIMEOUT! at %v\n", DEFAULT_TIME_OUT)
+	}
+}
