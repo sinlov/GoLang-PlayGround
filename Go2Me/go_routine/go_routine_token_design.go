e7c42c82fb96601a14e91a8005a5690611efa0ea
sinlov <sinlovgmppt@gmail.com>
2016-11-10 13:51:35 +0800

    add doc of token design

--- Go2Me/go_routine/go_routine_token_design.go
+++ Go2Me/go_routine/go_routine_token_design.go
@@ -9,8 +9,11 @@ import (
 )
 
 type TokenHeatBeat struct {
+	// Uid behavior
 	UId   chan int
+	// message behavior
 	Msg   chan string
+	// token check behavior
 	Token chan string
 }
 
@@ -22,16 +25,18 @@ func timeSedHash() string {
 	return fmt.Sprintf("%x", t.Sum(nil));
 }
 
+// init token info
 func NewTokenInfo() {
 	tokenStore.Msg = make(chan string, 10)
 	tokenStore.Token = make(chan string, 10)
 	tokenStore.UId = make(chan int, 10)
 	thisTime := time.Now()
-	tokenStore.UId <- thisTime.Minute() / 2
+	tokenStore.UId <- thisTime.Minute() % 2
 	tokenStore.Msg <- thisTime.String()
 	tokenStore.Token <- timeSedHash()
 }
 
+// you can set your update token logic
 func updateTokenInfo() {
 	thisTime := time.Now()
 	r := rand.New(rand.NewSource(thisTime.UnixNano()))
@@ -42,16 +47,19 @@ func updateTokenInfo() {
 	time.Sleep(time.Second * 1)
 }
 
+// ReadToken this is routine way update, reactive info by different behavior
 func ReadToken(key string) {
 	for i := 0; i < 10; i++ {
 		go updateTokenInfo()
 	}
 	select {
 	case t := <-tokenStore.Token:
+	// behavior token response
 		if key == t {
 			fmt.Printf("Token token %s, msg %s \n", <-tokenStore.Token, <-tokenStore.Msg)
 		}
 	case id := <-tokenStore.UId:
+	// behavior UId check response
 		if id > 0 {
 			fmt.Printf("UId token %s, msg %s \n", <-tokenStore.Token, <-tokenStore.Msg)
 		} else {
