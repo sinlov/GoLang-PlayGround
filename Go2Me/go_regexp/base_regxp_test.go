00a02a13cb2bd3b76889bbe4a2a898f72d38847c
sinlov <sinlovgmppt@gmail.com>
2016-11-14 10:55:10 +0800

    add golang regxp test

---
+++ Go2Me/go_regexp/base_regxp_test.go
@@ -0,0 +1,78 @@
+package go_regexp
+
+import "testing"
+
+func Test_chat(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name:"chat",
+			args:args{
+				text: `Hello 世界！123 Go.`,
+			},
+		},
+	}
+	for _, tt := range tests {
+		chat(tt.args.text)
+	}
+}
+
+func Test_innerStringFind(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name:"chat",
+			args:args{
+				text: `Hello 世界！123 Go.`,
+			},
+		},
+	}
+	for _, tt := range tests {
+		innerStringFind(tt.args.text)
+	}
+}
+
+func Test_punctuationMark(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name:"chat",
+			args:args{
+				text: `Hello 世界！123 Go.`,
+			},
+		},
+	}
+	for _, tt := range tests {
+		punctuationMark(tt.args.text)
+	}
+}
+
+func Test_replaceString(t *testing.T) {
+	type args struct {
+		text string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		replaceString(tt.args.text)
+	}
+}
