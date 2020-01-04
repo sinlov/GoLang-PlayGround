bd120375ccd8fff523acce81a849677cba3f3a9f
sinlov <sinlovgmppt@gmail.com>
2020-01-04 16:26:24 +0800

    add go_routines_execution_order_test.go

---
+++ Go2Me/go_routine/go_routines_execution_order_test.go
@@ -0,0 +1,16 @@
+package go_routine
+
+import "testing"
+
+func TestRoutinesOrderWithChan(t *testing.T) {
+	ch := make(chan int)
+	go func() {
+		t.Log("start goroutine")
+		ch <- 0
+		t.Log("done goroutine")
+	}()
+	t.Log("before goroutine")
+	<- ch
+	//close(ch)
+	t.Log("function done")
+}
